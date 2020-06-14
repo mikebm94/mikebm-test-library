c598a2f573e49b68d81bc51511a13b7cc3ddc51c
mikebm94 <mbmiller1994@outlook.com>
2020-06-13 23:07:25 -0500

    Random change to test duplicate release versions

--- build/serverbuild.ps1
+++ build/serverbuild.ps1
@@ -3,4 +3,4 @@ $versionString = "$($version.Major).$($version.Minor).$($version.Patch)"
 Write-Host "##vso[task.setvariable variable=mylibver]$versionString"
 
 & $PSScriptRoot\build.ps1 -version $versionString
-
+#
