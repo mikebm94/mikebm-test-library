1076a4df12dda0c5c2c5cdf982f5f246f4f7c033
mikebm94 <mbmiller1994@outlook.com>
2020-06-13 22:35:59 -0500

    Rename version job variable

--- build/serverbuild.ps1
+++ build/serverbuild.ps1
@@ -1,6 +1,6 @@
 $version = Get-Content "$PSScriptRoot\version.json" | Out-String | ConvertFrom-Json
 $versionString = "$($version.Major).$($version.Minor).$($version.Patch)"
-Write-Host "##vso[task.setvariable variable=versionstr;isSecret=false;isOutput=true;]$versionString"
+Write-Host "##vso[task.setvariable variable=mylibver]$versionString"
 
 & $PSScriptRoot\build.ps1 -version $versionString
 
